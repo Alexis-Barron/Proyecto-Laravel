ce24e9b966a31f67c962a08a687d228598016dfe
Alexis Barron <alexisrogelio6@gmail.com>
2024-10-30 18:29:23 -0600

    Base de datos

---
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Return_;
+
+use function Ramsey\Uuid\v1;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function index()
+    {
+        $post = Post::find(1);
+        $category = Category::find(1);
+        //$post = Post::find(3)->delete();
+        dd($category->posts[0]->title);
+
+        // $post->update(
+        //     [
+        //     'title' => 'test title new',
+        //     'slug' => 'test slug',
+        //     'conetnt' => 'test content',
+        //     'category_id' => 1,
+        //     'description' => 'test description',
+        //     'posted' => 'not',
+        //     'image' => 'test image',
+        //     ]
+        // );
+        //Post::create(
+          //   [
+            // 'title' => 'test title',
+             //'slug' => 'test slug',
+            // 'conetnt' => 'test content',
+            // 'category_id' => 1,
+             //'description' => 'test description',
+             //'posted' => 'not',
+            // 'image' => 'test image',
+            // ]
+         //);
+        return 'index';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\Dashboard\PostController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\Fragment\RoutableFragmentRenderer;
+use Symfony\Component\Routing\Annotation\Route as AnnotationRoute;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('Post', PostController::class);
