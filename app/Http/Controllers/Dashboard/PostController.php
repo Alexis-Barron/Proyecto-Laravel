24a48a33b0924786827b60ef9cb902ab135b5d22
Alexis Barron <alexisrogelio6@gmail.com>
2024-11-07 20:32:52 -0600

    aaa

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -8,142 +8,58 @@ use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
-use PhpParser\Node\Stmt\Return_;
-
-use function Ramsey\Uuid\v1;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-
     public function index()
     {
+        session()->flush();
+        //session(['key' => 'value']);
         $posts = Post::paginate(3);
         return view('dashboard.post.index', compact('posts'));
-
-    
-
-        // $post = Post::find(1);
-        // $category = Category::find(1);
-        // $post = Post::find(3)->delete();
-        // dd($category->posts[0]->title);
-        // $post->update(
-        //     [
-        //     'title' => 'test title new',
-        //     'slug' => 'test slug',
-        //     'conetnt' => 'test content',
-        //     'category_id' => 1,
-        //     'description' => 'test description',
-        //     'posted' => 'not',
-        //     'image' => 'test image',
-        //     ]
-        // );
-        //Post::create(
-          //   [
-            // 'title' => 'test title',
-             //'slug' => 'test slug',
-            // 'conetnt' => 'test content',
-            // 'category_id' => 1,
-             //'description' => 'test description',
-             //'posted' => 'not',
-            // 'image' => 'test image',
-            // ]
-         //);
-     
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        $categories = Category::pluck('id','title');
+        $categories = Category::pluck('id', 'title');
         $post = new Post();
         
-        return view('dashboard.post.create', compact('categories','post'));
-
-
+        return view('dashboard.post.create', compact('categories', 'post'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreRequest $request)
     {
         Post::create($request->validated());
-        return to_route('post.index');
-        // $request->validate([
-        //     'title' => 'required|min:5|max:500',
-        //     'slug' => 'required|min:5|max:500',
-        //     'content' => 'required|min:7',
-        //     'category_id' => 'required|integer',
-        //     'description' => 'required|min:7',
-        //     'posted' => 'required',
-        // ]);
-
-        // echo 'not';
-         /* Post::create(
-             [
-             'title' => $request->all()['title'],
-             'slug' =>$request->all()['slug'],
-             'content' => $request->all()['content'],
-             'category_id' => $request->all()['category_id'],
-             'description' => $request->all()['description'],
-             'posted' => $request->all()['posted'],
-             //'image' => $request->all()['image'],
-             ]
-         );
-
-         dd($request->all());*/
-
-
+        return to_route('post.index')->with('status', 'Post created');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Post $post)
     {
-        return view('dashboard.post.show',['post'=>$post]);
+        return view('dashboard.post.show', ['post' => $post]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Post $post)
     {
-        $categories = Category::pluck('id','title');
+        $categories = Category::pluck('id', 'title');
         return view('dashboard.post.edit', compact('categories', 'post'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(PutRequest $request, Post $post)
     {
         $data = $request->validated();
 
-        //image
-        if(isset($data['image'])){
-            $data ['image'] =$filaname= time().'.'.$data['image']->extension();
-
-            $request->image->move(public_path('uploads/posts'),$filaname);
+        if (isset($data['image'])) {
+            $data['image'] = $filename = time() . '.' . $data['image']->extension();
+            $request->image->move(public_path('uploads/posts'), $filename);
         }
-        //image
+
         $post->update($data);
-        return to_route('post.index');
+        return to_route('post.index')->with('status', 'Post updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Post $post)
     {
         $post->delete();
-        return to_route('post.index');
+        return to_route('post.index')->with('status', 'Post deleted');
     }
 }
-
-
--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -8,6 +8,15 @@
 </head>
 <body>
 
+    @session('key')
+    <h1>{{ $value }}</h1>
+    @endsession
+
+
+    @if (@session('status'))
+        {{ session ('status') }}
+    @endif
+
     @yield('content')
 
     <section>
